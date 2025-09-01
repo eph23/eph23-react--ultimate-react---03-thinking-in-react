99da7f596d236642d172d7725a7432e4ee8f5888
ephraim <ephraims.818@gmail.com>
2025-09-01 19:56:18 +0600

    REACT--ULTIMATE-REACT---THINKING-IN-REACT - 01. Thinking About State and Lifting State Up

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -10,9 +10,9 @@ function Logo() {
     return <h1>🌴Far Away🧳</h1>;
 }
 
-function Form() {
+function Form({ onAddItems }) {
     const [description, setDescription] = useState("");
-    const [quantity, setQuantity] = useState(10);
+    const [quantity, setQuantity] = useState(1);
 
     const handleSubmit = function (event) {
         event.preventDefault();
@@ -26,6 +26,8 @@ function Form() {
             id: Date.now(),
         };
         console.log(newItem);
+
+        onAddItems(newItem);
 
         setDescription("");
         setQuantity(1);
@@ -57,11 +59,11 @@ function Form() {
     );
 }
 
-function PackingList() {
+function PackingList({ items }) {
     return (
         <div className="list">
             <ul>
-                {initialItems.map((item) => (
+                {items.map((item) => (
                     <Item item={item} key={item.id} />
                 ))}
             </ul>
@@ -89,11 +91,17 @@ function Stats() {
 }
 
 function App() {
+    const [items, setItems] = useState([]);
+
+    function handleAddItems(item) {
+        setItems((items) => [...items, item]);
+    }
+
     return (
         <div className="app">
             <Logo />
-            <Form />
-            <PackingList />
+            <Form onAddItems={handleAddItems} />
+            <PackingList items={items} />
             <Stats />
         </div>
     );
