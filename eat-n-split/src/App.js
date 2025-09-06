beef4c85c279d884785e2e8b654373e5b80fee77
ephraim <ephraims.818@gmail.com>
2025-09-06 17:04:35 +0600

    REACT--ULTIMATE-REACT---THINKING-IN-REACT - [PROJECT - EAT-N-SPLIT] - 02. Building the Static App Forms

--- eat-n-split/src/App.js
+++ eat-n-split/src/App.js
@@ -46,12 +46,52 @@ function Friend({ friend }) {
                     {friend.name} owes you ${Math.abs(friend.balance)}
                 </p>
             )}
-            {friend.balance === 0 && (
-                <p className="red">You and {friend.name} are even</p>
-            )}
+            {friend.balance === 0 && <p>You and {friend.name} are even</p>}
 
-            <button className="button">Select</button>
+            <Button>Select</Button>
         </li>
+    );
+}
+
+function Button({ children }) {
+    return <button className="button">{children}</button>;
+}
+
+function FormAddFriend() {
+    return (
+        <form className="form-add-friend">
+            <label> Friend name</label>
+            <input type="text" />
+
+            <label>Image URL</label>
+            <input type="text" />
+
+            <Button>Add</Button>
+        </form>
+    );
+}
+
+function FormSplitBill() {
+    return (
+        <form className="form-split-bill">
+            <h2>Split a bill with</h2>
+            <label>💰Bill value</label>
+            <input type="text" />
+
+            <label>💸Your Expense</label>
+            <input type="text" />
+
+            <label>💰 X's expense</label>
+            <input type="text" disabled />
+
+            <label>🤑 Who is paying the bill </label>
+            <select>
+                <option value="user">You</option>
+                <option value="friend">X</option>
+            </select>
+
+            <Button>Split bill</Button>
+        </form>
     );
 }
 
@@ -60,7 +100,10 @@ function App() {
         <div className="app">
             <div className="sidebar">
                 <FriendsList />
+                <FormAddFriend />
+                <Button>Add friend</Button>
             </div>
+            <FormSplitBill />
         </div>
     );
 }
