5348e6fa1f17442b3979b7ea442cd9525eca29dc
ephraim <ephraims.818@gmail.com>
2025-09-07 15:21:15 +0600

    REACT--ULTIMATE-REACT---THINKING-IN-REACT - [PROJECT - EAT-N-SPLIT] - 04. Adding a New Friend

--- eat-n-split/src/App.js
+++ eat-n-split/src/App.js
@@ -21,9 +21,7 @@ const initialFriends = [
     },
 ];
 
-function FriendsList() {
-    const friends = initialFriends;
-
+function FriendsList({ friends }) {
     return (
         <ul>
             {friends.map((friend) => (
@@ -63,14 +61,44 @@ function Button({ children, onClick }) {
     );
 }
 
-function FormAddFriend() {
+function FormAddFriend({ onAddFriend }) {
+    const [name, setName] = useState("");
+    const [image, setImage] = useState("https://i.pravatar.cc/48");
+
+    const handleSubmit = function (event) {
+        event.preventDefault();
+
+        if (!name || !image) return;
+
+        const id = crypto.randomUUID();
+        const newFriend = {
+            name,
+            image: `${image}?=${id}`,
+            balance: 0,
+            id: id,
+        };
+
+        onAddFriend(newFriend);
+
+        setName("");
+        setImage("https://i.pravatar.cc/48");
+    };
+
     return (
-        <form className="form-add-friend">
+        <form className="form-add-friend" onSubmit={handleSubmit}>
             <label>🖌Friend name</label>
-            <input type="text" />
+            <input
+                type="text"
+                value={name}
+                onChange={(event) => setName(event.target.value)}
+            />
 
             <label>📷Image URL</label>
-            <input type="text" />
+            <input
+                type="text"
+                value={image}
+                onChange={(event) => setImage(event.target.value)}
+            />
 
             <Button>Add</Button>
         </form>
@@ -102,17 +130,25 @@ function FormSplitBill() {
 }
 
 function App() {
+    const [friends, setFriends] = useState(initialFriends);
     const [showAddFriend, setShowAddFriend] = useState(false);
 
     const handleShowAddFriend = function () {
         setShowAddFriend((show) => !show);
     };
 
+    const handleAddFriend = function (friend) {
+        setFriends((friends) => [...friends, friend]);
+        setShowAddFriend(false);
+    };
+
     return (
         <div className="app">
             <div className="sidebar">
-                <FriendsList />
-                {showAddFriend && <FormAddFriend />}
+                <FriendsList friends={friends} />
+                {showAddFriend && (
+                    <FormAddFriend onAddFriend={handleAddFriend} />
+                )}
                 <Button onClick={handleShowAddFriend}>
                     {showAddFriend ? "Close" : "Add friend"}
                 </Button>
